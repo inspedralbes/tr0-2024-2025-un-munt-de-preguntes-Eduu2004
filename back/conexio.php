c27992cd3d155414d121cb1916b93d933ab97584
[Eduu2004] <[a20edurenlop@inspedralbes.cat]>
2024-10-07 10:49:56 +0200

    commit

--- back/conexio.php
+++ back/conexio.php
@@ -1,14 +1,14 @@
 <?php
 
-//$servername = "localhost:8083";
-//$username = "a20edurenlop_bd"; // Your database username
-//$password = "Ausias123";     // Your database password
-//$dbname = "a20edurenlop_peli"; // Your database name
+$servername = "localhost:8083";
+$username = "a20edurenlop_bd"; // Your database username
+$password = "Ausias123";     // Your database password
+$dbname = "a20edurenlop_peli"; // Your database name
 
-$servername = "localhost";
-$username = "phpmyadmin"; // Your database username
-$password = "usuario";     // Your database password
-$dbname = "peli"; // Your database name
+//$servername = "localhost";
+//$username = "phpmyadmin"; // Your database username
+//$password = "usuario";     // Your database password
+//$dbname = "peli"; // Your database name
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
